d6e03c7d69fd654d07fcf34d5e1f95d850dea37f
MartaSch <martaschab27@gmail.com>
2023-04-05 19:10:29 +0200

    homework-11

--- src/clearGallery.js
+++ src/clearGallery.js
@@ -1,6 +1,7 @@
 const galleryPhoto = document.querySelector(".gallery");
 const searchBtn = document.querySelector('[type = "submit"]');
 const loadMoreB = document.querySelector(".load-more");
+let page = 1;
 export function clearGallery() {
     page = 1
     galleryPhoto.innerHTML='';
