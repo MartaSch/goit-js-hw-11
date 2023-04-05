192635ae138406736ac861e5bf7007caf9865328
MartaSch <martaschab27@gmail.com>
2023-04-05 13:16:05 +0200

    homework-11

--- src/createGallery.js
+++ src/createGallery.js
@@ -7,13 +7,17 @@ export function createGallery(images) {
     <img class="photo-card-img" src="${webformatURL}" onclick = "return false" alt="${tags}" load="lazy"/>
     </a><div class="info">
     <p class="info-item">
-    <b>Likes:</b> ${likes}</p>
+    <b>Likes:</b> 
+    ${likes}</p>
     <p class="info-item">
-    <b>Views:</b> ${views}</p>
+    <b>Views:</b> 
+    ${views}</p>
     <p class="info-item">
-    <b>Comments:</b> ${comments}</p>
+    <b>Comments:</b> 
+    ${comments}</p>
     <p class="info-item">
-    <b>Downloads:</b> ${downloads}</p>
+    <b>Downloads:</b> 
+    ${downloads}</p>
     </div>
     </div>`
     }).join("");
